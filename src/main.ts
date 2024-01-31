8242f5769b4a3e4c42b1f672ff4103662a36f830
yannamaria <156942643+yannamaria@users.noreply.github.com>
2024-01-31 17:41:36 -0300

    criacão de rota

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,24 @@ import { db, firestore} from '../banco_de_dados/firebase';
 
 const app = express();
 
+app.get("/", (req, res)=>{
+    res.send("bem vindo a minha primeira API");
+})
+
+app.post("/usario", async(req, res)=>{
+    const usuario = req.body.nome
+
+    try{
+        const docRef= await firestore. addDoc(firestore. collection(db,"usuario"),{
+            nome: nome,
+        })
+        res.send(docRef.id)
+    }catch(e){
+        console.log(e)
+        res.status(500).send(e)
+    }
+    
+})
 app.listen(3000, function(){
     console.log('servidor rodado na porta http://localhost:3000')
 });
