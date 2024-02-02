611d6318fc2e62e6fdb5630ffd931a6523bd0b93
yannamaria <156942643+yannamaria@users.noreply.github.com>
2024-02-02 13:41:06 -0300

    primeiro usuario

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,24 @@ import express from "express"
 import { db, firestore} from '../banco_de_dados/firebase';
 
 const app = express();
-
+ app.use(express.json())
 app.get("/", (req, res)=>{
     res.send("bem vindo a minha primeira API");
 })
 
-app.post("/usario", async(req, res)=>{
-    const usuario = req.body.nome
+app.post("/usuario", async(req, res)=>{
+    const nome = req.body.nome;
+    const email = req .body.email
+    const telefone = req.body.telefone
 
     try{
         const docRef= await firestore. addDoc(firestore. collection(db,"usuario"),{
             nome: nome,
+            email: email,
+            telefone: telefone,
+            
         })
-        res.send(docRef.id)
+        res.send("usuario adicionado com sucesso:" + docRef.id);
     }catch(e){
         console.log(e)
         res.status(500).send(e)
